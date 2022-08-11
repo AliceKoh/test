17367dd270af477d890dd39071bafaf3bc3db42f
AliceKoh <109507439+AliceKoh@users.noreply.github.com>
2022-08-11 20:08:50 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import streamlit as st
-from PIL import Image
-#import streamlit.components.v1 as components
 import pandas as pd
 #import numpy as np
-from prognosis_app import run_prognosis_app
+
 
 			
 result = {
@@ -18,4 +16,5 @@ st.write(answer)
 
 answer = pd.DataFrame(result,index=[0])
 answer.to_csv('data/answer.csv',index=False)
+st.write(answer)
 
